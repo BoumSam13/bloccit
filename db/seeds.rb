643150cda3d9a4cffb9e7625ede201c1c27d060a
boumsam <sam_boumeester41@hotmail.com>
2016-02-17 03:18:25 +0000

    Assignment 30 Seeding: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,14 @@ posts = Post.all
     )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A title", body: "A body")
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Comment.find_or_create_by(body: "A comment body", post: posts)
+puts "#{Comment.count}"
+
 puts "Seed finished"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
