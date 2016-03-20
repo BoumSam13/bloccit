18b8297a53107a25de9bcf6c7ed3cffaa22061b2
boumsam <sam_boumeester41@hotmail.com>
2016-03-20 16:28:34 +0000

    Assignment 37 signingUp: completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@
    resources :sponsored_posts, except: [:index]
    end
    resources :users, only: [:new, :create]
+   post 'users/confirm' => 'users#confirm'
    get 'about' => 'welcome#about'
    root 'welcome#index'
  end
