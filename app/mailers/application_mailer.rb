6bdbd0c200effd9a544c05d36f40e78aa80e0e48
boumsam <sam_boumeester41@hotmail.com>
2016-05-16 20:21:15 +0000

    Checkpoint 44 favorites: completed

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "sam_boumeester41@hotmail.com"
+  layout 'mailer'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    
     before_save { self.email = email.downcase }
     before_save { self.role ||= :member }
     before_save :format_name
@@ -27,5 +29,9 @@ class User < ActiveRecord::Base
             end
             self.name = name_array.join(" ")
         end
+    end
+    
+    def favorite_for(post)
+        favorites.where(post_id: post.id).first
     end
 end
